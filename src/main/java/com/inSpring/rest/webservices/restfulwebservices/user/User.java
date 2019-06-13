8c6614d77c8b7a54a8a0d5bc6eece809ab5e594e
shambhavitewari <33371065+shambhavitewari@users.noreply.github.com>
2019-06-13 18:55:39 +0530

    Second Commit

--- src/main/java/com/inSpring/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/inSpring/rest/webservices/restfulwebservices/user/User.java
@@ -1,26 +1,44 @@
 package com.inSpring.rest.webservices.restfulwebservices.user;
 
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
+@ApiModel(description = "All details about the user.")
 @Entity
 public class User {
 
     @Id
     @GeneratedValue
-    private Integer id;
+    private int id;
 
+    @Size(min=2, message= "Name should be of min 2 characters")
+    @ApiModelProperty(notes= "Name should have atleast 2 characters")
     private String name;
 
+
+    @Past
+    @ApiModelProperty(notes="Birth date should be in the past")
     private Date birthdate;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
     protected User(){
 
     }
 
-    public User(Integer id, String name, Date birthdate) {
+    public User(Integer id, String name, Date birthdate)
+    {
         super();
         this.id = id;
         this.name = name;
@@ -35,6 +53,13 @@ public class User {
         this.birthdate = birthdate;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -44,13 +69,20 @@ public class User {
         this.name = name;
     }
 
-    public Integer getId() {
-        return id;
+    public List<Post> getPosts() {
+        return posts;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
-
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
+    }
 }
